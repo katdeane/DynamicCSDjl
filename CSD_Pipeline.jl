ae8cb7b7e717c722a8450c8f1717db94cfc14e81
kdeane <katrina.deane@lin-magdeburg.de>
2020-07-07 16:40:39 +0200

    data saved per animal with JLD into Data folder, group loop was added, need callGroup for all groups and other output for final Dict output

--- CSD_Pipeline.jl
+++ CSD_Pipeline.jl
@@ -5,6 +5,7 @@ using Plots
 using MAT
 using DataFrames
 using Colors
+using JLD
 
 # get to correct directory and label it home
 home    = @__DIR__
@@ -12,6 +13,7 @@ raw     = joinpath(home,"raw")
 func    = joinpath(home,"functions")
 figs    = joinpath(home,"figs")
 group   = joinpath(home,"groups")
+datap   = joinpath(home,"Data")
 
 include(joinpath(func,"Dynamic_CSD.jl"))
 include(joinpath(func,"SingleTrialCSD.jl"))
@@ -21,35 +23,48 @@ include(joinpath(func,"functions.jl"))
 include(joinpath(group,"callGroup.jl"))
 
 # determine data to read -- this will be a more complicated process later!
-# groups, animal, condition, measurement
-Group = "KIT"
-#'Pre' 'preAM' 'preAMtono' 'preCL' 'preCLtono' 'spPre1' 'spPost1' 'CL' 'CLtono' 'spPre2' 'spPost2' 'AM' 'AMtono' 'spEnd'
-
+Group = ["KIC" "KIT" "KIV"]
+#"Pre" "preAM" "preAMtono" "preCL" "preCLtono" "spPre1" "spPost1" "CL" "CLtono" "spPre2" "spPost2" "AM" "AMtono" "spEnd"
 CondName = ["Pre" "CL"]
-# generate lists of channels, layers, and measurements for each animal in this group
-animalList,chanList,LIIList,LIVList,LVList,LVIList,CondList = callGroup(Group) # in groups folder
 
-# loop through each animal (dictionary per animal)
-for iAn = 1:length(animalList)
-    # loop through each type of measurement condition (specified by CondName)
-    for iCond = 1:length(CondName)
-        # loop through each measurement within that condition for that animal
-        for iMeas = 1:length(CondList[CondName[iCond]][iAn])
+# loop through groups
+for iGr = 1:length(Group)
+    # generate lists of channels, layers, and measurements for each animal in this group
+    animalList,chanList,LIIList,LIVList,LVList,LVIList,CondList = callGroup(Group[iGr]) # in groups folder
 
-            #to test!
-            # iAn, iCond, iMeas = 1, 1, 1
-            channels = chanList[iAn]
-            LII  = LIIList[iAn]
-            LIV  = LIVList[iAn]
-            LV   = LVList[iAn]
-            LVI  = LVIList[iAn]
-            # #
-            measurement = animalList[iAn] * "_" * CondList[CondName[iCond]][iAn][iMeas] * ".mat"
-            println("Analyzing measurement: " * measurement[1:end-4])
+    # loop through each animal (dictionary per animal)
+    Animal = Dict()
+    for iAn = 1:length(animalList)
 
-            csdData,snkData = Dynamic_CSD(measurement,channels,LII,LIV,
-                LV,LVI,raw,figs,Group)
+        AnimalName = animalList[iAn]
 
+        # loop through each type of measurement condition (specified by CondName)
+        Condition = Dict()
+        for iCond = 1:length(CondName)
+            # loop through each measurement within that condition for that animal
+            Measurement = Dict()
+            for iMeas = 1:length(CondList[CondName[iCond]][iAn])
+
+                channels = chanList[iAn]
+                LII  = LIIList[iAn]
+                LIV  = LIVList[iAn]
+                LV   = LVList[iAn]
+                LVI  = LVIList[iAn]
+
+                measurement = AnimalName * "_" * CondList[CondName[iCond]][iAn][iMeas] * ".mat"
+                println("Analyzing measurement: " * measurement[1:end-4])
+
+                csdData,snkData = Dynamic_CSD(measurement,channels,LII,LIV,
+                    LV,LVI,raw,figs,Group);
+
+                Measurement[CondList[CondName[iCond]][iAn][iMeas]] = csdData,snkData;
+            end
+            Condition[CondName[iCond]] = Measurement
         end
+        Animal[AnimalName] = Condition
+        name = joinpath(datap,AnimalName) * "_Data.jld"
+        save(name, Animal[AnimalName])
     end
 end
+
+# to load: Data  = load("D:\\Julia\\DynamicCSDjl\\Data\\KIT05_Data.jld")
